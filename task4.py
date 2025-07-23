f0190434cfcdb438b009d5790ed9831679a1a457
sabira97 <sbirabbasova1@gmail.com>
2025-07-23 17:32:40 +0400

    task4

---
+++ task4.py
@@ -0,0 +1,8 @@
+birinci_eded = 7
+ikinci_eded = 8
+if birinci_eded > ikinci_eded:
+    print(f"birinci_eded:{birinci_eded} boyukdur ikinci_eded:{ikinci_eded}")
+elif birinci_eded < ikinci_eded:
+    print(f"birinci_eded:{birinci_eded} kicikdir ikinci_eded:{ikinci_eded}")
+else:
+    print(f"birinci_eded:{birinci_eded} beraberdir ikinci_eded:{ikinci_eded}")
