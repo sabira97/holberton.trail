fb222d17ad04ea59018e885c356504026e2f1609
sabira97 <sbirabbasova1@gmail.com>
2025-08-07 17:53:21 +0400

    task12.py

--- task12.py
+++ task12.py
@@ -1,12 +1,37 @@
-class Person:
-    def __init__(self, age=0):
-        self._age = age #protected atribut
-    def get_age(self):
-        return self._age
-    def set_age(self, value):
-        if value < 0:
-            raise ValueError("Age cannot be negative")
-        self._age = value
-p = Person()
-p.set_age(27)
-print(p.get_age())  # 27
+class Kitab:
+    def __init__(self, ad, muellif):
+        self.ad = ad
+        self.muellif = muellif
+        self.goturulub = False
+
+class Kitabxana:
+    def __init__(self):
+        self.kitablar = []
+
+    def kitab_elave_et(self, kitab):
+        self.kitablar.append(kitab)
+
+    def butun_kitablar(self):
+        for k in self.kitablar:
+            status = "Goturulub" if k.goturulub else "Movcuddur"
+            print(f"{k.ad} - {k.muellif} [{status}]")
+
+    def kitab_gotur(self, ad):
+        for k in self.kitablar:
+            if k.ad == ad and not k.goturulub:
+                k.goturulub = True
+                print(f"{ad} goturuldu.")
+                return
+            print("Kitab tapilmadi ve ya goturulub.")
+
+k1 = Kitab("Aktirisa", "Xez Paltolu Maddonna")
+k2 = Kitab("Cinayet ve Ceza", "Qurur ve Qerez")
+
+lib = Kitabxana()
+lib.kitab_elave_et(k1)
+lib.kitab_elave_et(k2)
+
+lib.butun_kitablar()
+lib.kitab_gotur("Cinayet ve Ceza")
+lib.butun_kitablar()
+    
